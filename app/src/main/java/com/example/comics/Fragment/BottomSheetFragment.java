f2a5eb962cfdca7720bffede4dbcc070b711a122
MinhKhang1002 <hmkhang2000@gmail.com>
2022-03-11 19:06:52 +0700

    add bottomsheetfragment

--- app/src/main/java/com/example/comics/Fragment/BottomSheetFragment.java
+++ app/src/main/java/com/example/comics/Fragment/BottomSheetFragment.java
@@ -1,6 +1,7 @@
 package com.example.comics.Fragment;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,18 +13,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.comics.Adapter.BottomSheetAdapter;
+import com.example.comics.Models.Book;
 import com.example.comics.R;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
     View view;
+    Context context;
+
     RecyclerView recyclerView;
+    Book book = new Book();
+    BottomSheetAdapter bottomSheetAdapter;
+    public BottomSheetFragment(Context context, Book book){
+        this.context = context;
+        this.book=book;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
          view= inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
         recyclerView = view.findViewById(R.id.recycleViewChapterList);
+
          return view;
     }
 
@@ -33,6 +45,7 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         BottomSheetDialog bottomSheetDialog= (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
         View viewDialog = LayoutInflater.from(getContext()).inflate(R.layout.fragment_bottom_sheet,null);
         bottomSheetDialog.setContentView(viewDialog);
+        bottomSheetAdapter = new BottomSheetAdapter(book, getContext());
         return bottomSheetDialog;
     }
 
